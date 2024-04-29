45620924b789cb2f493fa1b5c3cf2d6ecaaddae8
bem130 <haruki070617@gmail.com>
2024-04-29 12:37:20 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> cmds = new List<string>(); ;
+        cmds.AddRange(System.Environment.GetCommandLineArgs());
+        cmds.RemoveAt(0);
+        if (cmds.Count() > 0)
+        {
+            var text = File.ReadAllText(cmds[0], Encoding.GetEncoding("shift-jis"));
+            BdebtTools.Proc(text.Replace("\r\n", "\n").Split('\n'), args);
+        }
+        else
+        {
+            BdebtTools.Proc(LoadInput(Console.In), args);
+        }
+        return;
+    }
+    static string[] LoadInput(System.IO.TextReader data)
+    {
+        string[] lines = new string[1];
+        string? line;
+        int num = 0;
+        while ((line = data.ReadLine()) != null)
+        {
+            if (num >= lines.Length)
+            {
+                Array.Resize(ref lines, lines.Length * 2);
+            }
+            lines[num] = line;
+            num++;
+        }
+        Array.Resize(ref lines, num);
+        return lines;
+    }
+}
---
+++ run.bat
@@ -0,0 +1,3 @@
+dotnet run
+@rem ildasm main.exe /out=main.il
+@rem main.exe
